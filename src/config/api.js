145ce23b94a7e1244176a2a780c884d42b88623f
Sanjay Balam <b.sanjay0701@gmail.com>
2026-02-20 14:41:42 +0530

    fixes

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "https://rej-server-2ayv.onrender.com",
 });
 
 // Auto-attach auth token to every request
